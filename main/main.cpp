35bd967e677355f45433bbbdcff63cc39cde8417
Julien Lebot <julien.ar.lebot@gmail.com>
2024-01-30 03:51:57 +0100

    Bring it all together

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,24 @@
 #include <cstdint>
+#include <charconv>
 #include <esp_system.h>
 #include <esp_timer.h>
+#include <esp_event.h>
 #include <driver/gpio.h>
+#include <nvs_flash.h>
+#include <esp_netif.h>
+
 #include "device.hpp"
 #include "esp32s3.hpp"
 #include "spi_lcd.hpp"
+#include "wifi.hpp"
+#include "mqtt.hpp"
 #include "app_ui.hpp"
 
 typedef Device<ESP32S3> ESPDevice;
 static ESPDevice dev;
 static SpiLcd<ESPDevice> lcd;
 static AppUi ui;
+static MQTTClient mqtt;
 
 void lcd_spi_pre_transfer_callback(spi_transaction_t *t)
 {
@@ -46,8 +54,35 @@ void app_main(void)
     lv_display_t * disp = lv_display_create(ESPDevice::DeviceDefinitions::screen_width, ESPDevice::DeviceDefinitions::screen_height);
     lv_display_set_flush_cb(disp, disp_flush);
     lv_display_set_buffers(disp, buf, NULL, sizeof(buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    ui.init();
 
-    lvgl_app_main();
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    WifiClient wifi;
+    wifi.connect("HOME_LEGACY", "cra2qm5q");
+
+    mqtt.connect([](esp_mqtt_client_config_t &cfg) {
+    });
+    mqtt.subscribe("power_meter/phase_1_power", [](auto payload)
+    {
+        float power = 0;
+        std::from_chars(payload.data(), payload.data() + payload.length(), power);
+        ui.set_phase_power(0, power);
+    });
+    mqtt.subscribe("power_meter/phase_2_power", [](auto payload)
+    {
+        float power = 0;
+        std::from_chars(payload.data(), payload.data() + payload.length(), power);
+        ui.set_phase_power(1, power);
+    });
+    mqtt.subscribe("power_meter/phase_3_power", [](auto payload)
+    {
+        float power = 0;
+        std::from_chars(payload.data(), payload.data() + payload.length(), power);
+        ui.set_phase_power(2, power);
+    });
 
     const esp_timer_create_args_t lvgl_tick_timer_args = {
       .callback = &lvgl_tick,
